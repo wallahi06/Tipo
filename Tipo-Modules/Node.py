8dadfe2ff84fee4bd995d787f72c6f11e7bfd99f
Liam Nordvall <86798194+wallahi06@users.noreply.github.com>
2022-06-17 19:28:26 +0200

    Update Node.py

--- Tipo-Modules/Node.py
+++ Tipo-Modules/Node.py
@@ -1,12 +1,17 @@
 import numpy as np
 from tipo.Functional import activation, loss
 
+
 class LinearPass:
-    def __init__(self, num_inputs, num_neurons):
+    def __init__(self, num_inputs, num_neurons, bias=True):
+        self.bias = bias
         self.weights = 0.10 * np.random.randn(num_inputs, num_neurons)
         self.biases = np.zeros((1, num_neurons))
 
     def passData(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
-
-
+        if self.bias:
+            output = np.dot(inputs, self.weights) + self.biases
+            return output
+        else:
+            output = np.dot(inputs, self.weights)
+            return output
