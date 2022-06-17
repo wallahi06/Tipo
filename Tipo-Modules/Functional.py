2af82005c502d02794e38d5c468507d303d3dc6b
Liam Nordvall <86798194+wallahi06@users.noreply.github.com>
2022-06-17 19:25:29 +0200

    Update Functional.py

--- Tipo-Modules/Functional.py
+++ Tipo-Modules/Functional.py
@@ -1,5 +1,5 @@
 # This code is under MIT License and free to use as it is, the code have been written by Liam Nordvall 
-# togheter with Lord Hummer. The module includes most of the functions needed for everytinh from basic
+# together with Lord Hummer. The module includes most of the functions needed for everything from basic
 # Neural Networks to more deep networks with many layers and nodes.
 
 
@@ -7,7 +7,7 @@ import numpy as np
 
 
 # This class consists of all the necessary activation functions needed, we have put them into different
-# methods to make it easy to use togheter with other modules. We made the methods ready to use but also 
+# methods to make it easy to use together with other modules. We made the methods ready to use but also 
 # added additional parameters for our professional users.
 
 class ActivationFunctions:
@@ -15,62 +15,59 @@ class ActivationFunctions:
     def __init__(self):
         pass
 
-    def binarystep(value):
-        value = np.array(value)
-        return np.heaviside(value, 1)
+    def binarystep(self, values):
+        values = np.array(values, dtype=float)
+        data = np.heaviside(values, 1)
+        return data
 
-    def linear(self, value, gradient=4):
-        value = np.array(value)
-        return gradient*value
+    def linear(self, values, gradient=2):
+        return np.array(values, dtype=float)*gradient
 
-    def sigmoid(self, value):
-        value = np.array(value)
-        return 1/(1 + np.exp(-value))
+    def sigmoid(self, values):
+        values = np.array(values)
+        data = 1 / (1 + np.exp(-values))
+        return data
 
-    def tanh(self, value):
-        value = np.array(value)
-        return 2/(1 + np.exp(-2*value)) - 1
+    def tanh(self, values):
+        values = np.array(values)
+        data = 2/(1 + np.exp(-2*values)) - 1
+        return data
 
-    def relu(self, value, gradient=1):
-        value = np.array(value)
-        if value.all() < 0:
-            return 0
-        else:
-            return value*gradient
+    def relu(self, values, gradient=1):
+        values = np.array(values, dtype=float)
+        data = np.where(values > 0, values*gradient, 0)
+        return data
 
-    def leakyrelu(self, value, gradient=1, leak=0.01):
-        value = np.array(value)
-        if value < 0:
-            return value*leak
-        else:
-            return value*gradient
+    def leakyrelu(self, values, gradient=1, leak=0.01):
+        values = np.array(values, dtype=float)
+        data = np.where(values > 0, values*gradient, values*leak)    
+        return data                      
 
-    def elu(self, value, a=0.1):
-        value = np.array(value)
-        if value < 0:
-            return a*(np.exp(value)-1)
-        else:
-            return value
+    def elu(self, values, a=0.1, gradient=1):
+        values = np.array(values, dtype=float)
+        data = np.where(values > 0, values*gradient, a*(np.exp(values)-1))
+        return data
 
-    def swish(self, value):
-        value = np.array(value)
-        return value/(1-np.exp(-value))
+    def swish(self, values):
+        values = np.array(values, dtype=float)
+        data = values/(1-np.exp(-values))
+        return data
 
-    def softmax(self, value):
-        z = np.exp(value)
-        value_ = z / z.sum()
-        return value_
+    def softmax(self, values):
+        values = np.array(values)
+        data = np.exp(values) / np.exp(values).sum()
+        return data
 
 
-# We initilize the class with a keyword for easier use, namely, instead of writing activation = ActivationFunctions() 
+# We initialize the class with a keyword for easier use, namely, instead of writing activation = ActivationFunctions() 
 # you just import activation class from Tipo.Functional to get access to all the activation functions
 
 activation = ActivationFunctions()
 
 
-# This class includes all the important Loss and Cost functions, we have made the use of these funcitons
-# much easier by naming the methods to relevant names and using few parameters but at the same time giving more parameters for
-# fine tuning to our professional users.
+# This class includes all the important Loss and Cost functions, we have made the use of these functions
+# much easier by naming the methods to relevant names and using few parameters but at the same time giving 
+# more parameters for fine-tuning to our professional users.
 
 class LossFunctions:
 
